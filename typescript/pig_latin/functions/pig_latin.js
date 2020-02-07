1f789e3d14db6dce551f53594b396de92574f177
biomiller <92millero@gmail.com>
2020-02-07 14:58:24 +0000

    working on pig latin logic

--- typescript/pig_latin/functions/pig_latin.js
+++ typescript/pig_latin/functions/pig_latin.js
@@ -1,8 +1,35 @@
 "use strict";
 exports.handler = function (event, context, callback) {
     var body = JSON.parse(event.body);
+    var sentence = body.sentence;
+    var vowels = ["a", "e", "i", "o", "u"];
+    var word;
+    var letter;
+    var index = 0;
+    var response_string = "";
+    for (var _i = 0, _a = sentence.split(" "); _i < _a.length; _i++) {
+        word = _a[_i];
+        var prefix = [];
+        for (var _b = 0, _c = word.split(""); _b < _c.length; _b++) {
+            letter = _c[_b];
+            if (vowels.includes(letter.toLowerCase())) {
+                response_string = response_string + word + "yay" + " ";
+                break;
+            }
+            // else if (vowels.includes(letter.toLowerCase())){
+            //     response_string = response_string + word + prefix.join("") +"ay" + " "
+            //     break
+            // }
+            else if (!vowels.includes(letter.toLowerCase())) {
+                prefix.push(letter);
+                word = word.substring(1);
+            }
+            index += 1;
+        }
+    }
+    ;
     callback(null, {
         statusCode: 200,
-        body: JSON.stringify(body)
+        body: response_string
     });
 };
--- typescript/pig_latin/functions_ts/pig_latin.ts
+++ typescript/pig_latin/functions_ts/pig_latin.ts
@@ -1,14 +1,44 @@
 exports.handler = function(event: { body: string; }, context: any, callback: (arg0: null, arg1: { statusCode: number; body: string; }) => void) {
 
-    const body:string = JSON.parse(event.body);
+    interface sentence {
+        sentence: string;
+    }
+    const body:sentence = JSON.parse(event.body);
+
+    const sentence:string = body.sentence;
+
+    const vowels: string[] = ["a", "e", "i", "o", "u"];
+
+    let word:string;
+    let letter:string;
+    let index:number = 0;
+    let response_string: string = "";
+
+
+    for (word of sentence.split(" ")){
+        
+        let prefix: string[] = [];
+        
+        for(letter of word.split("")){
+            
+            if (index == 0 && vowels.includes(letter.toLowerCase())){
+                response_string = response_string + word + "yay"+ " "
+                break
+            }
+            else if (vowels.includes(letter.toLowerCase())){
+                response_string = response_string + word + prefix.join("") +"ay" + " "
+                break
+            }
+            else if (!vowels.includes(letter.toLowerCase())){
+                prefix.push(letter)
+                word = word.substring(1)
+            }
+            index += 1
+        }
+    };
 
     callback(null, {
         statusCode: 200,
-        body: JSON.stringify(body)
+        body: response_string
     });
-
-
-
-
-
 }
