1115d2ea929b43efe6a38e8b92046de91c40de28
biomiller <92millero@gmail.com>
2020-02-17 17:39:05 +0000

    returning array of random letter strings

--- typescript/wordsearch/functions/wordsearch.js
+++ typescript/wordsearch/functions/wordsearch.js
@@ -2,10 +2,33 @@
 var fs = require('fs');
 exports.handler = function (event, context, callback) {
     var wordArray = fs.readFileSync('./node_modules/word-list/words.txt', 'utf8').split('\n');
-    var r = Math.random().toString(36).substring(2, 10);
-    // wordArray.slice(0,10).toString()
+    var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    var rows = [];
+    var wordsFound = [];
+    var string_length = 30;
+    for (var n = 0; n < 10; n++) {
+        var randomString = '';
+        for (var i = 0; i < string_length; i++) {
+            var rnum = Math.floor(Math.random() * alphabet.length);
+            randomString += alphabet[rnum];
+        }
+        rows.push(randomString);
+    }
+    var word;
+    var row;
+    for (var _i = 0, wordArray_1 = wordArray; _i < wordArray_1.length; _i++) {
+        word = wordArray_1[_i];
+        for (var _a = 0, rows_1 = rows; _a < rows_1.length; _a++) {
+            row = rows_1[_a];
+            if (row.includes(word)) {
+                wordsFound.push(word);
+            }
+        }
+    }
+    ;
+    var response = { rows: rows, wordsFound: wordsFound };
     callback(null, {
         statusCode: 200,
-        body: r
+        body: JSON.stringify(response)
     });
 };
--- typescript/wordsearch/functions_ts/wordsearch.ts
+++ typescript/wordsearch/functions_ts/wordsearch.ts
@@ -4,12 +4,43 @@ exports.handler = function (event: any, context: any, callback: any) {
 
     const wordArray: string[] = fs.readFileSync('./node_modules/word-list/words.txt', 'utf8').split('\n');
 
-    let r: string = Math.random().toString(36).substring(2,10);
+    const alphabet: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-    // wordArray.slice(0,10).toString()
-    
+    let rows: string[] = [];
+    let wordsFound: string[] = [];
+    let string_length: number = 30;
+
+    interface response {
+        rows: string[];
+        wordsFound: string[];
+    }
+
+    for (let n = 0; n < 10; n++) {
+        let randomString: string = '';
+
+        for (let i = 0; i < string_length; i++) {
+            let rnum: number = Math.floor(Math.random() * alphabet.length);
+            randomString += alphabet[rnum];
+        }
+
+        rows.push(randomString);
+    }
+
+    let word: string;
+    let row: string;
+
+    for (word of wordArray) {
+        for (row of rows) {
+            if (row.includes(word)) { 
+                wordsFound.push(word) 
+            }
+        }
+    };
+
+    let response: response = { rows: rows, wordsFound: wordsFound }
+
     callback(null, {
         statusCode: 200,
-        body: r
+        body: JSON.stringify(response)
     });
 };
